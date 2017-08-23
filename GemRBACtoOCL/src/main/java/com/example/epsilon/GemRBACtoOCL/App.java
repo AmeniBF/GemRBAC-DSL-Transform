647e75713a8fb6ac5357b484b6f51f2ed8761553
ameni benfadhel <ameni.benfadhel@uni.lu>
2017-08-23 16:43:05 +0200

    initial commit

---
+++ GemRBACtoOCL/src/main/java/com/example/epsilon/GemRBACtoOCL/App.java
@@ -0,0 +1,13 @@
+package com.example.epsilon.GemRBACtoOCL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
